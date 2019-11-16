5273bf1939f025986268515d2afd0c81ac6d4a03
SenierLink <1325181337@qq.com>
2019-11-16 14:59:47 +0800

    尝试书写code

---
+++ php/class/Messages.php
@@ -0,0 +1,19 @@
+<?php
+namespace \App\myClass\Messages;
+
+/**
+ * Class Messages
+ * @package App\myClass\Messages
+ */
+class Messages
+{
+    private $messages = [];
+    private $fetchPoint = 0;
+
+    public function __construct()
+    {
+
+    }
+
+
+}
